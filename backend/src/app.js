28589a33dc597d3224a463d76321b7c34794d1be
Fernando Blanco <fernandoblancovaldez@gmail.com>
2023-12-13 03:25:01 -0300

    adding gitinore file and installing some basic dependences like express, cors, helmet morgan nodemoon. script dev also added to run nodemon when app.js does it

---
+++ backend/src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+app.use(helmet());
+app.use(morgan("dev"));
