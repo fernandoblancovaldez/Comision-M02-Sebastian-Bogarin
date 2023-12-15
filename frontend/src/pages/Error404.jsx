1b8100e43045337741479c222d05dde2ec7cdfe9
Fernando Blanco <fernandoblancovaldez@gmail.com>
2023-12-15 11:28:33 -0300

    feat: frontend-routes-protect with route <outlet> element. AuthGlobalContext/PrivateRoutes and Posts pages created. make user sesion persist by local storage

--- frontend/src/pages/Error404.jsx
+++ frontend/src/pages/Error404.jsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Error404 = () => {
   return (
     <>
       <h3>Error404</h3>
       <p>Not Found</p>
+      <Link to="/">Go to Home</Link>
     </>
   );
 };
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,16 @@
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+
 import LoginForm from "../components/LoginForm";
 import RegisterForm from "../components/RegisterForm";
 
 const Home = () => {
+  const { auth, logout } = useContext(AuthContext);
+  if (auth === undefined) return <div>Loading ..</div>;
   return (
     <>
-      <h3>Home</h3>
+      <h3>{auth?.user?.username}</h3>
+      <button onClick={logout}>Logout</button>
       <LoginForm />
       <RegisterForm />
     </>
