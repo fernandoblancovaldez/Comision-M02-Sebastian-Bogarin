b95efc264550746f4ef989ed0c530e691290fbea
Fernando Blanco <fernandoblancovaldez@gmail.com>
2023-12-15 04:47:10 -0300

    feat: frontend-register-form-functionality is done, also removed some useless react importations in jsx files

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Error404 from "../pages/Error404";
 import Home from "../pages/Home";
--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -1,14 +1,52 @@
-import React from "react";
+import { useRef } from "react";
 
 const RegisterForm = () => {
+  const formRef = useRef(null);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    const username = formData.get("username"),
+      password = formData.get("password"),
+      email = formData.get("email"),
+      avatarURL = formData.get("avatarURL");
+
+    const user = { username, password, email, avatarURL };
+
+    const req = await fetch(
+      "https://api-tripshare.up.railway.app/user/register",
+      {
+        method: "POST",
+        body: JSON.stringify(user),
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+    if (req.status !== 201) return alert("Error al registrar al usuario");
+
+    const res = await req.json();
+    console.log(res);
+
+    formRef.current.reset();
+  };
+
   return (
     <>
       <h2>Register</h2>
-      <form>
-        <input type="text" placeholder="Usuario" required />
-        <input type="password" placeholder="******" required />
-        <input type="email" placeholder="usuario@usuario.com" required />
-        <input type="url" placeholder="www.my-avatar.com" required />
+      <form onSubmit={handleSubmit} ref={formRef}>
+        <input type="text" placeholder="Usuario" name="username" required />
+        <input type="password" placeholder="******" name="password" required />
+        <input
+          type="email"
+          placeholder="usuario@usuario.com"
+          name="email"
+          required
+        />
+        <input
+          type="url"
+          placeholder="www.my-avatar.com"
+          name="avatarURL"
+          required
+        />
         <button>Register</button>
       </form>
     </>
